b9a4f03098bccf1e0285d5823d035ae0eeb36228
Попов Артём <vip.artem.popov.14@mail.ru>
2021-12-05 16:48:03 +0500

    Добавление новой библиотеки из другого проекта того же репозитория. + Обновлнение комментариев на форме Task4

---
+++ Is-1-19-PopovAE/ConnectBD/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // Я подключил библиотеку Windows.Forms, чтобы иметь возможность выводить информацию из кода этого класса в меседж бокс
+
+namespace ConnectBD
+{
+    public class ClassConnectBD
+    {
+        public string stringconn_DB = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+
+        public void ConnectInfo()
+        {
+            MessageBox.Show(stringconn_DB);
+        }
+    }
+}
--- Is-1-19-PopovAE/Task4.cs
+++ Is-1-19-PopovAE/Task4.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ConnectDB;
 using MySql.Data.MySqlClient;
+using ConnectBD; // добавляем дополнительную библиотеку с классом из другого проекта (предварительно привязав его по ссылкам)
 
 namespace Is_1_19_PopovAE
 {
@@ -19,21 +19,27 @@ namespace Is_1_19_PopovAE
             InitializeComponent();
         }
 
+        
         private void Form5_Load(object sender, EventArgs e)
         {
             //Большую часть кода реюзнул из прошлого задания
-            Connector_DB conn4 = new Connector_DB();
+
+            // Создаём экземпляр класса, добавленного из другого проекта того же репозитория
+            ClassConnectBD conn4 = new ClassConnectBD();
             MySqlConnection connect = new MySqlConnection(conn4.stringconn_DB);
+            // Переменная содержит запрос На вывод столбцов из таблицы t_datetime
             string sql = $"SELECT idStud, fioStud, drStud FROM t_datetime";
             try
             {
-                connect.Open();
+                connect.Open();// Открываем соединение
 
                 MySqlDataAdapter IDataAdapter = new MySqlDataAdapter(sql, connect);
 
+                // Создаём виртуальную таблицу
                 DataSet dataset = new DataSet();
 
-                IDataAdapter.Fill(dataset);
+                IDataAdapter.Fill(dataset);// заполняем витруальную таблицу 
+                // предаём датагриду вид заполненной нами виртуальной таблицы
                 dataGridView1.DataSource = dataset.Tables[0];
             }
             catch
@@ -42,25 +48,31 @@ namespace Is_1_19_PopovAE
             }
             finally
             {
-                connect.Close();
+                connect.Close();// Закрываем соединение
             }
         }
         string id_rows5 = "0";
+
+        // Данный метод будет срабатывать по нажатию кнопки мыши на датагрид
         private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
+            /* Это условие срботает только тогда, когда мы нажимаем на какое-либо поле, отображаемое 
+            в датагриде, а так же если мы нажали на это поле левой кнопкой мыши */
             if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Left))
             {
+                // определяется, какая именно строка была нами нажата
                 dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
-
+                // выбранная строка подсвечивается выделяется программой
                 dataGridView1.CurrentRow.Selected = true;
-
+                
                 string index_rows5;
-
+                // В переменную попадает уникальный индекс ячейки, которую мы выбрали нажатием
                 index_rows5 = dataGridView1.SelectedCells[0].RowIndex.ToString();
 
+                // Переменной, которую мы объявили глобально присваивается значение второй по счёту ячейки из выбранной нами строки
                 id_rows5 = dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString();
-                DateTime x = DateTime.Today;
-                DateTime y = Convert.ToDateTime(dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString());
+                DateTime x = DateTime.Today; // Программа получает в х настоящую на данный момент дату
+                DateTime y = Convert.ToDateTime(dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString());// в y попадает дата и время ячейки, полученной из таблицы
                 string resultDays = (x - y).ToString(); //Тут хранится значение, которое соотвутствует прошедшим дням
                 MessageBox.Show("Со дня рождения прошло " + resultDays.Substring(0, resultDays.Length - 9) + " дней"); //Тут это значение приводится в презентабельный вид и выводится пользователю
             }
